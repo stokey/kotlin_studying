158fa9d10cd5748c6d3b4a43de6d00c01ac0be7b
stokey <stokeygen@hotmail.com>
2017-05-24 22:23:01 +0800

    feat: init project

---
+++ app/src/main/java/com/stokey/kotlin/JavaClass.java
@@ -0,0 +1,8 @@
+package com.stokey.kotlin;
+
+/**
+ * Created by stokey on 2017/5/24.
+ */
+
+public class JavaClass {
+}
---
+++ app/src/main/java/com/stokey/kotlin/kotlin/HelloWorld.kt
@@ -0,0 +1,5 @@
+package com.stokey.kotlin.kotlin
+
+/**
+ * Created by stokey on 2017/5/24.
+ */
